878018a67ee6b2e954864b1e7372a516479e7dbf
DanMB <daniel@setsnail.com>
2022-05-24 10:29:00 +0200

    scripts path fixes

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
-const fileName = './app/neutralino.config.json';
+const fileName = path.resolve(__dirname, '../app/neutralino.config.json');
 const config = require(fileName);
+
+const isProd = !process.argv.includes('--dev');
+const envFile = path.resolve(__dirname, '../.env.' + (isProd ? 'prod' : 'dev'));
+require('dotenv').config({ path: envFile });
+
+const verDir = path.resolve(__dirname, '../build/ver');
+if (!fs.existsSync(verDir)) fs.mkdirSync(verDir);
 
 const url = new URL(process.env['URL']);
 url.pathname = url.pathname + 'ver/update.neu';
@@ -13,11 +21,16 @@ let manifest = {
 	data: {},
 };
 
-let data = JSON.stringify(manifest);
-fs.writeFileSync('build/ver/manifest.json', data);
+const manifestPath = path.resolve(__dirname, '../build/ver/manifest.json');
+
+let data = JSON.stringify(manifest, null, '\t');
+fs.writeFileSync(manifestPath, data);
 console.log('manifest.json was written');
 
-fs.copyFileSync(`app/dist/${config.cli.binaryName}/resources.neu`, 'build/ver/update.neu');
+const resourcesPath = path.resolve(__dirname, `../app/dist/${config.cli.binaryName}/resources.neu`);
+const updatePath = path.resolve(__dirname, '../build/ver/update.neu');
+
+fs.copyFileSync(resourcesPath, updatePath);
 console.log('resources.neu was copied');
 console.log('');
 console.log('post-build.js successful!');
--- scripts/set-config.js
+++ scripts/set-config.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-const fileName = './app/neutralino.config.json';
+const path = require('path');
 
+const fileName = path.resolve(__dirname, '../app/neutralino.config.json');
 const config = require(fileName);
 
 const isProd = !process.argv.includes('--dev');
-
-let envFile = './.env.' + (isProd ? 'prod' : 'dev');
+const envFile = path.resolve(__dirname, '../.env.' + (isProd ? 'prod' : 'dev'));
 require('dotenv').config({ path: envFile });
 
 const url = new URL(process.env['URL']);
@@ -14,8 +14,9 @@ url.searchParams.set('isneu', 'true');
 config.port = parseInt(process.env['NEU_PORT']);
 config.url = url;
 
-fs.writeFileSync(fileName, JSON.stringify(config));
+fs.writeFileSync(fileName, JSON.stringify(config, null, '\t'));
 
 console.log('updated conig');
 console.log('');
 console.log('set-config.js successful!');
+console.log('');
