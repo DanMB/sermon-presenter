7c54155787580c6379617f3d8a5e2b1086af2eb9
DanMB <daniel@setsnail.com>
2022-08-02 15:54:35 +0200

    improved neu config

--- scripts/set-config.js
+++ scripts/set-config.js
@@ -1,28 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-const fileName = path.resolve(__dirname, '../neutralino.config.json');
-const config = require(fileName);
-
 const isProd = !process.argv.includes('--dev');
 const envFile = path.resolve(__dirname, '../.env.' + (isProd ? 'prod' : 'dev'));
 require('dotenv').config({ path: envFile });
 
-const htmlPath = path.resolve(__dirname, '../src/index.html');
+const url = new URL(process.env['URL']);
+const urlString = url.toString();
+
+const htmlPath = path.resolve(__dirname, '../src/public/client.html');
 let htmlData = fs.readFileSync(htmlPath, 'utf8');
-htmlData = htmlData.replaceAll(/(?<=localhost:)\d+(?=\/.+neutralino\.js")/gi, process.env['NEU_PORT']);
+htmlData = htmlData.replaceAll(/https?:\/\/.*\/(?=\?port)/gi, urlString);
 fs.writeFileSync(htmlPath, htmlData);
 console.log('updated html');
 
-// if (parseInt(config.port) !== parseInt(process.env['NEU_PORT']) || config.url !== url.toString()) {
-// 	const url = new URL(process.env['URL']);
-// 	config.port = parseInt(process.env['NEU_PORT']);
-// 	config.url = url.toString();
-
-// 	fs.writeFileSync(fileName, JSON.stringify(config, null, '\t'));
-// }
-
-console.log('updated config');
 console.log('');
 console.log('set-config.js successful!');
 console.log('');
