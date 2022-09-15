3f337b4597000595b562a943d01eb22c13efbc9c
Daniel <daniel@setsnail.com>
2022-09-15 15:18:26 +0200

    added checkbox input

---
+++ src/components/Input/CheckboxInput.tsx
@@ -0,0 +1,37 @@
+import { h, JSX } from 'preact';
+import { Ref, useEffect, useState } from 'preact/hooks';
+import './Input.scss';
+
+export interface IProps extends Omit<JSX.HTMLAttributes<HTMLInputElement>, 'onChange' | 'defaultValue'> {
+	label: string;
+	defaultValue?: boolean;
+	onChange?: (value: boolean) => void;
+	inputRef?: Ref<HTMLInputElement>;
+}
+
+const CheckboxInput = ({ className, label, defaultValue, onChange, inputRef, type, ...restProps }: IProps) => {
+	const [currentValue, setCurrentValue] = useState<boolean>(defaultValue ?? false);
+
+	useEffect(() => {
+		if (onChange) onChange(currentValue);
+	}, [currentValue]);
+
+	return (
+		<input
+			{...restProps}
+			class={`Input ${className}`}
+			type={'checkbox'}
+			ref={inputRef}
+			checked={currentValue}
+			aria-label={label}
+			onChange={e => {
+				setCurrentValue((e?.target as HTMLInputElement)?.checked ?? false);
+			}}
+			onKeyUp={e => {
+				setCurrentValue((e?.target as HTMLInputElement)?.checked ?? false);
+			}}
+		/>
+	);
+};
+
+export default CheckboxInput;
