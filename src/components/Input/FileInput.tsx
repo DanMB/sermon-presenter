2a16a67836b4a4d118c14c05d50c6ccf97922782
Daniel <daniel@setsnail.com>
2022-10-06 16:10:15 +0200

    added fileinput component

---
+++ src/components/Input/FileInput.tsx
@@ -0,0 +1,71 @@
+import { h, JSX } from 'preact';
+import { Ref, useEffect, useRef, useState } from 'preact/hooks';
+import Input, { IProps } from './Input';
+import Music from '@src/components/icons/Music';
+
+export interface IFileData {
+	lastModified: number;
+	name: string;
+	type: string;
+	data: string | ArrayBuffer;
+}
+
+export interface IFileInputProps extends Omit<IProps, 'onChange' | 'defaultValue'> {
+	defaultValue?: IFileData | null;
+	onChange?: (value: IFileData | null) => void;
+}
+
+const FileInput = ({ className, defaultValue, onChange, ...restProps }: IFileInputProps) => {
+	const [currentValue, setCurrentValue] = useState<IFileData | null>(defaultValue ?? null);
+	const data = useRef<File | null>(null);
+	const input = useRef<HTMLInputElement | null>(null);
+	const reader = useRef(new FileReader());
+
+	useEffect(() => {
+		if (onChange) onChange(currentValue);
+	}, [currentValue]);
+
+	useEffect(() => {
+		reader.current.addEventListener('load', onFileLoaded);
+
+		return function () {
+			reader.current.removeEventListener('load', onFileLoaded);
+		};
+	}, [reader]);
+
+	const onFileLoaded = (e: ProgressEvent<FileReader>) => {
+		if (e.target?.result && data.current) {
+			setCurrentValue({
+				lastModified: data.current?.lastModified,
+				name: data.current?.name,
+				type: data.current?.type,
+				data: e.target?.result,
+			});
+		} else {
+			setCurrentValue(null);
+		}
+	};
+
+	const internalChange = () => {
+		if (reader.current && input.current?.files && input.current?.files[0]) {
+			data.current = input.current?.files[0];
+			reader.current.readAsDataURL(data.current);
+		} else {
+			data.current = null;
+		}
+	};
+
+	return (
+		<Input
+			{...restProps}
+			inputRef={input}
+			type='file'
+			accept='image/jpeg, image/png, image/jpg'
+			value={currentValue?.name}
+			data-value={currentValue?.name ?? 'none'}
+			onChange={internalChange}
+		/>
+	);
+};
+
+export default FileInput;
