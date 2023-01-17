6ce419668cbcd0cf0e57b7a2b3030e6a3da205c8
Daniel <daniel@setsnail.com>
2023-01-17 11:13:38 +0100

    added song search functionality

--- src/modules/AddMusicModule/AddMusicModule.tsx
+++ src/modules/AddMusicModule/AddMusicModule.tsx
@@ -6,21 +6,42 @@ import Plus from '@src/components/icons/Plus';
 import Tabs, { useTabs } from '@src/ts/tabs/Tabs';
 import SearchInput from '@src/components/Input/SearchInput';
 import OurPraise from '@src/ts/OurPraise';
+import ISearchHit from '@src/types/ISearchHit';
+import Spinner from '@src/components/icons/Spinner';
 
 const AddMusicModule = () => {
+	const [loadingQuery, setLoadingQuery] = useState<string>('');
+	const [hits, setHits] = useState<ISearchHit[]>([]);
+
 	const onSearch = (value: string) => {
-		// console.log(value);
-		// OurPraise
-		// 	?.search(value)
-		// 	.then(results => {
-		// 		console.log(results);
-		// 	});
+		setLoadingQuery(value.trim());
 	};
+
+	useEffect(() => {
+		if (!!loadingQuery) {
+			OurPraise?.search(loadingQuery).then(results => {
+				if (results.query === loadingQuery) {
+					setLoadingQuery('');
+					setHits(results.hits);
+				}
+			});
+		}
+	}, [loadingQuery]);
 
 	return (
 		<div class='AddMusicModule'>
 			<div class='title'>Songs</div>
 			<SearchInput label={'Search songs'} placeholder={'Search songs'} onChange={onSearch} />
+			{!!loadingQuery && <Spinner />}
+			{!loadingQuery &&
+				hits &&
+				hits.map(song => (
+					<div class='searchHit' key={song.path}>
+						<div class='name'>{song.title}</div>
+						<div class='author'>{song.authors}</div>
+						<div class='icon'></div>
+					</div>
+				))}
 		</div>
 	);
 };
