66c9410aaddc5ee1c2f9e2f7fee681c68488afc1
Daniel <daniel@setsnail.com>
2023-01-19 13:57:00 +0100

    fixed functions

--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -26,52 +26,10 @@ const Song = ({ song, index, listId }: { song: IOurPraiseSong; index?: number; l
 			  )
 			: [];
 
-		const moveFocus = (dir: 1 | -1, end: boolean = false) => {
-			const active = document.activeElement;
-			if (!slidesRef.current) return;
-
-			if (!active || container.current === active) {
-				let focusOn = 0;
-				if (dir === -1) focusOn = items.length - 1;
-				if (items[focusOn]) (items[focusOn] as HTMLElement)?.focus();
-				return;
-			}
-
-			if (active && !slidesRef.current.contains(active)) return;
-
-			for (let i = 0; i < items.length; i++) {
-				const item = items[i];
-				if (item === active || item?.contains(active)) {
-					let focusOn = i + dir;
-					if (end) {
-						if (dir === 1) focusOn = items.length - 1;
-						else focusOn = 0;
-					} else {
-						if (focusOn >= items.length) focusOn = 0;
-						else if (focusOn < 0) focusOn = items.length - 1;
-					}
-					(items[focusOn] as HTMLElement)?.focus();
-					break;
-				}
-			}
-		};
-
 		const keydown = (e: KeyboardEvent) => {
 			if (!slidesRef.current) return;
 
-			if ((e.key === 'ArrowUp' || e.key === 'ArrowLeft') && !e.ctrlKey) {
-				e.preventDefault();
-				moveFocus(-1);
-			} else if ((e.key === 'ArrowDown' || e.key === 'ArrowRight') && !e.ctrlKey) {
-				e.preventDefault();
-				moveFocus(1);
-			} else if (e.key === 'Home' || ((e.key === 'ArrowUp' || e.key === 'ArrowLeft') && e.ctrlKey)) {
-				e.preventDefault();
-				moveFocus(-1, true);
-			} else if (e.key === 'End' || ((e.key === 'ArrowDown' || e.key === 'ArrowRight') && e.ctrlKey)) {
-				e.preventDefault();
-				moveFocus(1, true);
-			} else if (digitExp.test(e.code)) {
+			if (digitExp.test(e.code)) {
 				let focusOn = parseInt(e.key) - 1;
 				if (focusOn === -1) focusOn = 9;
 				if (e.shiftKey) focusOn = +10;
@@ -88,23 +46,15 @@ const Song = ({ song, index, listId }: { song: IOurPraiseSong; index?: number; l
 			}
 		};
 
-		// if (container.current) container.current.addEventListener('keydown', keydown);
+		if (container.current) container.current.addEventListener('keydown', keydown);
 
 		return function () {
-			// if (container.current) container.current.removeEventListener('keydown', keydown);
+			if (container.current) container.current.removeEventListener('keydown', keydown);
 		};
 	}, []);
 
 	const onClick = (e: h.JSX.TargetedEvent<HTMLDivElement>) => {
-		// console.log('onClick', e.currentTarget.id);
 		CustomEvents.dispatch(Events.SLIDE, e.currentTarget.id);
-	};
-
-	const onFocus = (e: h.JSX.TargetedEvent<HTMLDivElement>) => {
-		// console.log('onFocus', e.currentTarget.id, PresentWindow.get()?.current !== e.currentTarget.id);
-		// if (PresentWindow.get()?.current !== e.currentTarget.id) {
-		// 	CustomEvents.dispatch(Events.SLIDE, e.currentTarget.id);
-		// }
 	};
 
 	return (
@@ -120,7 +70,6 @@ const Song = ({ song, index, listId }: { song: IOurPraiseSong; index?: number; l
 						{...{ slide, index, listId }}
 						songId={song.title}
 						onClick={onClick}
-						onFocus={onFocus}
 					/>
 				))}
 			</div>
