35b452b5f125d29ae306bdf228a2dc80852d0ee7
Daniel <daniel@setsnail.com>
2023-04-05 16:55:35 +0200

    svg tsx fixes

--- src/components/icons/Close.tsx
+++ src/components/icons/Close.tsx
@@ -7,9 +7,9 @@ const Close = (props: SVGProps<SVGSVGElement>) => {
 			viewBox='0 0 24 24'
 			fill='none'
 			stroke='currentColor'
-			stroke-width='2'
-			stroke-linecap='round'
-			stroke-linejoin='round'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
 			{...props}
 		>
 			<line x1='18' y1='6' x2='6' y2='18'></line>
--- src/components/icons/Video.tsx
+++ src/components/icons/Video.tsx
@@ -5,9 +5,9 @@ const Video = () => {
 			viewBox='0 0 24 24'
 			fill='none'
 			stroke='currentColor'
-			stroke-width='2'
-			stroke-linecap='round'
-			stroke-linejoin='round'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
 		>
 			<rect x='2' y='2' width='20' height='20' rx='2.18' ry='2.18'></rect>
 			<line x1='7' y1='2' x2='7' y2='22'></line>
