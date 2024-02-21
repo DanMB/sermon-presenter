9530f3bcebf637e2bf9053e747a943a682ecb120
Daniel <daniel@setsnail.com>
2024-02-21 22:38:32 +0100

    dan-ui

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	compilerOptions: {
 		// runes: true,
 	},
 	kit: {
 		adapter: adapter(),
+		alias: {
+			'@dan-ui/internal': './src/dan-ui/internal/*',
+			'@dan-ui': './src/dan-ui/components/*',
+		},
 	},
 	files: {
 		assets: 'src/static',
